0954fd400844a38d8af50f61bfa1fcb004d8a682
CatsterDaCoder <102768577+CatsterDaCoder@users.noreply.github.com>
2026-05-19 17:40:22 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/initiative-bard/",
   build: {
     target: "esnext",
   },
   server: {
-    // Allow embedding in OBR iframe
     headers: {
       "Cross-Origin-Embedder-Policy": "credentialless",
     },
